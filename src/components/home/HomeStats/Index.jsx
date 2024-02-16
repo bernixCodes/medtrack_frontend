3eaa5c345307a1c1b64d16e48f58bb18f87528f2
bernixCodes <adjoa.bernie@gmail.com>
2024-02-16 23:12:43 +0000

    count-up

--- src/components/home/HomeStats/Index.jsx
+++ src/components/home/HomeStats/Index.jsx
@@ -1,25 +1,44 @@
+import CountUp, { useCountUp } from "react-countup";
 import "./homestats.css";
+
 const HomeStats = () => {
+  useCountUp({
+    ref: "counter",
+    end: 1234567,
+    enableScrollSpy: true,
+    scrollSpyDelay: 1500,
+  });
+
   return (
     <div className="homestats">
       <div className="homestat-main">
         <div className="homestat-info">
-          <h3>29</h3>
+          <h3>
+            <CountUp end={39} enableScrollSpy />
+          </h3>
           <h4>Years of experience</h4>
         </div>
 
         <div className="homestat-info">
-          <h3>4099</h3>
+          <h3>
+            <CountUp end={4099} enableScrollSpy />
+          </h3>
           <h4>Satisfied Patients</h4>
         </div>
 
         <div className="homestat-info">
-          <h3>25</h3>
+          <h3>
+            {" "}
+            <CountUp end={89} enableScrollSpy />
+          </h3>
           <h4> Hospital rooms</h4>
         </div>
 
         <div className="homestat-info">
-          <h3>181</h3>
+          <h3>
+            {" "}
+            <CountUp end={301} enableScrollSpy />
+          </h3>
           <h4>Qualified doctors</h4>
         </div>
       </div>
